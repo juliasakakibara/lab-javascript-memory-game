cb638d7e5838a3ba370e40b1b9aa87338a6f3fb7
juliasakakibara <addller@Addllers-MacBook-Air.local>
2021-08-03 22:57:08 -0300

    memory.js done

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,43 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if(this.cards === undefined) {
+      return undefined;
+    }
+    const min = 0;
+    const max = this.cards.length;
+    let randomNumber;
+
+    this.cards.forEach(() => {
+      randomNumber = Math.floor(Math.random() * (max - min)) + min; 
+      
+      const selectedCard = this.cards[randomNumber];
+
+      this.cards.splice(randomNumber, 1);
+      this.cards.push(selectedCard)
+    });
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
-  }
+  if(card1 === card2) {
+    this.pairsGuessed++;
+    return true;
+    }
+    this.pairsClicked++
+  return false;
+}
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } 
+    return false
   }
 }
 
